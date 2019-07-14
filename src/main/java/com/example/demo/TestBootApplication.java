26357a44f81cac64775f456bd27843db28ae640d
lixinglong123 <995205046@qq.com>
2019-07-14 23:47:49 +0800

    test changes

--- src/main/java/com/example/demo/TestBootApplication.java
+++ src/main/java/com/example/demo/TestBootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TestBootApplication.class, args);
     }
-
 }
